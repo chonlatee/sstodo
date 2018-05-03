c15c96b8db3ffeeb5f71726046d55428bcce557a
Chonlatee <chonlatee@Helpsters-MacBook-Pro.local>
2018-05-03 23:55:42 +0700

    add index router and set env port

--- main.go
+++ main.go
@@ -1,14 +1,30 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
 	r := gin.Default()
+	r.Use(gin.Logger())
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "ok",
+		})
+	})
 
-	r.Run(":3000")
+	r.Run(":" + port)
 }
