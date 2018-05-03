142b6fcdae64d0766d85d57e5e0337062c29a7b1
Chonlatee <chonlatee@Helpsters-MacBook-Pro.local>
2018-05-04 00:12:34 +0700

    add line bot sdk

--- main.go
+++ main.go
@@ -5,10 +5,17 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 func main() {
+
 	port := os.Getenv("PORT")
+	bot, err := linebot.New(
+		os.Getenv("channel_secret"),
+		os.Getenv("channel_token")
+	)
+
 
 	if port == "" {
 		log.Fatal("$PORT must be set")
@@ -24,6 +31,30 @@ func main() {
 		c.JSON(200, gin.H{
 			"message": "ok",
 		})
+	})
+
+	r.GET("/callback", func (c *gin.Context) {
+		events, err := bot.ParseRequest(c.Request)
+		if err != nil {
+			if err == linebot.ErrInvalidSignature{
+				c.Header(400, "parse error")
+			} else {
+				c.Header(500, "server error")
+			}
+			return
+		}
+
+		for _, event := range events {
+			if event.Type == linebot.EventTypeMessage {
+				switch message := event.Message.(type) {
+				case *linebot.TextMessage:
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+						log.Print(err)
+					}
+				}
+			}
+		}
+
 	})
 
 	r.Run(":" + port)
